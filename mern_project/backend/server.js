98119fdf8cafaabc752b847bfb1e3edaa271d368
dacicus75 <dacicus75@hotmail.com>
2020-01-31 10:55:11 -0500

    added user model

--- mern_project/backend/server.js
+++ mern_project/backend/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-require("dotenv"). config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log("Server running on port: ${port}");
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://dacicus75:mern123456@cluster0-shard-00-00-l3xux.gcp.mongodb.net:27017,cluster0-shard-00-01-l3xux.gcp.mongodb.net:27017,cluster0-shard-00-02-l3xux.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+MongoClient.connect(uri, function(err, client) {
+  const collection = client.db("test").collection("devices");
+  console.log("MongoDB database connection established successfully");
+  // perform actions on the collection object
+  client.close();
 });
 
+
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+// );
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
+
+// const exercisesRouter = require('./routes/exercises');
+// const usersRouter = require('./routes/users');
+
+// app.use('/exercises', exercisesRouter);
+// app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
